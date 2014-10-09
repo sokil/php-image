b5fa86debf63bc6864597f55f1e719ab6d10fdaf
Dmytro Sokil <dmytro.sokil@gmail.com>
2014-10-09 10:07:05 +0300

    compatibility with php 5.3

--- tests/Image/ColorModel/RgbTest.php
+++ tests/Image/ColorModel/RgbTest.php
@@ -6,12 +6,12 @@ class RgbTest extends \PHPUnit_Framework_TestCase
 {
     public function testFromHex()
     {       
-        $this->assertEquals([239, 239, 239, 0], Rgb::fromHex('#efefef')->toArray());
+        $this->assertEquals(array(239, 239, 239, 0), Rgb::fromHex('#efefef')->toArray());
         
-        $this->assertEquals([239, 239, 239, 0], Rgb::fromHex('efefef')->toArray());
+        $this->assertEquals(array(239, 239, 239, 0), Rgb::fromHex('efefef')->toArray());
         
-        $this->assertEquals([239, 239, 239, 64], Rgb::fromHex('#80efefef')->toArray());
+        $this->assertEquals(array(239, 239, 239, 64), Rgb::fromHex('#80efefef')->toArray());
         
-        $this->assertEquals([239, 239, 239, 64], Rgb::fromHex('80efefef')->toArray());
+        $this->assertEquals(array(239, 239, 239, 64), Rgb::fromHex('80efefef')->toArray());
     }
 }
